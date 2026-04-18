9e31a9392598d8d58d88215282553ff2ed5d806b
saraouertani <sara.ouertani@esprit.tn>
2026-04-18 16:47:56 +0100

    feat: fix CORS and field reservation features

--- StreetLeague/src/main/java/com/example/streetleague/security/SecurityConfig.java
+++ StreetLeague/src/main/java/com/example/streetleague/security/SecurityConfig.java
@@ -152,7 +152,7 @@ public class SecurityConfig {
 
         // Méthodes HTTP autorisées (OPTIONS obligatoire pour les requêtes CORS preflight)
 
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE" ,"PATCH","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
