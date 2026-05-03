6561186c49c52d8504b7f779dda4210c25b2a180
Malak-Messaoui <Malak.Messaoui@esprit.tn>
2026-05-03 18:21:06 +0100

    AppConfig

--- StreetLeague/src/main/java/com/example/streetleague/config/AppConfig.java
+++ StreetLeague/src/main/java/com/example/streetleague/config/AppConfig.java
@@ -1,20 +1,13 @@
 package com.example.streetleague.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.Duration;
-
 @Configuration
 public class AppConfig {
-
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder
-                .setConnectTimeout(Duration.ofSeconds(3))   // ✅ setConnectTimeout
-                .setReadTimeout(Duration.ofSeconds(4))       // ✅ setReadTimeout
-                .build();
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
