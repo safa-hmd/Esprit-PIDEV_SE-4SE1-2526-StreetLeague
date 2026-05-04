c1153e82975fe76a162be7212ace601b56274341
Malak-Messaoui <Malak.Messaoui@esprit.tn>
2026-05-04 20:45:24 +0100

    app-prodv2

--- StreetLeague/src/main/java/com/example/streetleague/domain/User.java
+++ StreetLeague/src/main/java/com/example/streetleague/domain/User.java
@@ -15,10 +15,10 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Getter          // ← @Getter + @Setter au lieu de @Data
-@Setter          // ← @Data cause des conflits avec @Builder
-@NoArgsConstructor
-@AllArgsConstructor
+//@Getter          // ← @Getter + @Setter au lieu de @Data
+//@Setter          // ← @Data cause des conflits avec @Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class User {
@@ -112,4 +112,227 @@ public class User {
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     @JsonIgnore
     private SpinResult spinResult;
+
+
+    public Long getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Long idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<Team> getCaptainedTeams() {
+        return captainedTeams;
+    }
+
+    public void setCaptainedTeams(List<Team> captainedTeams) {
+        this.captainedTeams = captainedTeams;
+    }
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(List<Team> teams) {
+        this.teams = teams;
+    }
+
+    public List<Training> getTrainings() {
+        return trainings;
+    }
+
+    public void setTrainings(List<Training> trainings) {
+        this.trainings = trainings;
+    }
+
+    public List<Match> getCreatedMatches() {
+        return createdMatches;
+    }
+
+    public void setCreatedMatches(List<Match> createdMatches) {
+        this.createdMatches = createdMatches;
+    }
+
+    public String getResetToken() {
+        return resetToken;
+    }
+
+    public void setResetToken(String resetToken) {
+        this.resetToken = resetToken;
+    }
+
+    public LocalDateTime getResetTokenExpiry() {
+        return resetTokenExpiry;
+    }
+
+    public void setResetTokenExpiry(LocalDateTime resetTokenExpiry) {
+        this.resetTokenExpiry = resetTokenExpiry;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public List<waterReminder> getWaterReminders() {
+        return waterReminders;
+    }
+
+    public void setWaterReminders(List<waterReminder> waterReminders) {
+        this.waterReminders = waterReminders;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public Double getBmi() {
+        return bmi;
+    }
+
+    public void setBmi(Double bmi) {
+        this.bmi = bmi;
+    }
+
+    public List<DailyWaterLog> getWaterLogs() {
+        return waterLogs;
+    }
+
+    public void setWaterLogs(List<DailyWaterLog> waterLogs) {
+        this.waterLogs = waterLogs;
+    }
+
+    public List<UserGoal> getGoals() {
+        return goals;
+    }
+
+    public void setGoals(List<UserGoal> goals) {
+        this.goals = goals;
+    }
+
+    public List<UserBadge> getBadges() {
+        return badges;
+    }
+
+    public void setBadges(List<UserBadge> badges) {
+        this.badges = badges;
+    }
+
+    public SpinResult getSpinResult() {
+        return spinResult;
+    }
+
+    public void setSpinResult(SpinResult spinResult) {
+        this.spinResult = spinResult;
+    }
+
+    public User(Long idUser, String fullName, String email, String password, int age, Role role, boolean enabled, List<Team> captainedTeams, List<Team> teams, List<Training> trainings, List<Match> createdMatches, String resetToken, LocalDateTime resetTokenExpiry, Long teamId, List<Post> posts, List<Comment> comments, List<waterReminder> waterReminders, Double weight, Double height, Double bmi, List<DailyWaterLog> waterLogs, List<UserGoal> goals, List<UserBadge> badges, SpinResult spinResult) {
+        this.idUser = idUser;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.age = age;
+        this.role = role;
+        this.enabled = enabled;
+        this.captainedTeams = captainedTeams;
+        this.teams = teams;
+        this.trainings = trainings;
+        this.createdMatches = createdMatches;
+        this.resetToken = resetToken;
+        this.resetTokenExpiry = resetTokenExpiry;
+        this.teamId = teamId;
+        this.posts = posts;
+        this.comments = comments;
+        this.waterReminders = waterReminders;
+        this.weight = weight;
+        this.height = height;
+        this.bmi = bmi;
+        this.waterLogs = waterLogs;
+        this.goals = goals;
+        this.badges = badges;
+        this.spinResult = spinResult;
+    }
+
+    public User() {
+    }
 }
